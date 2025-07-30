eb3b3cfa8589e7e930b87f303549d811b2c7e571
zh <again.really.plz@gmail.com>
2025-07-30 21:56:59 +1000

    Automated commit from zen-ham at 2025-07-30 21:56:59.204055

--- src/zhmiscellanyocr/__init__.py
+++ src/zhmiscellanyocr/__init__.py
@@ -1,3 +1,4 @@
 from ._ocr_supportfuncs import batch_ocr
 from ._ocr_supportfuncs import ocr
 from ._ocr_supportfuncs import ptpath as pytesseract_exe
+from ._ocr_supportfuncs import set_tesseract_path
